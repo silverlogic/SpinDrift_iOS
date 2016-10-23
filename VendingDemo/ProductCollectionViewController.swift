e448ec81797e91385c68d13722438985c1c0cef8
Cristina <ce@tsl.io>
2016-10-22 23:14:51 -0400

    adding new products UI with fake data

---
+++ VendingDemo/ProductCollectionViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ProductCollectionViewController.swift
+//  
+//
+//  Created by Cristina Escalante on 10/22/16.
+//
+//
+
+import UIKit
+import Moya
+import CoreLocation
+
+import UIKit
+
+private let reuseIdentifier = "ProductCell"
+
+class ProductCollectionViewController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 5
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> ProductCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ProductCell
+    
+        // Configure the cell
+        cell.configureCell()
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+}
+
+
+
